704124aa13a5eb6e0040dd43b086941a51fedb34
MAbdullah-Riaz <abdullah.riaz@ocloudsolutions.net>
2023-01-26 20:14:26 +0500

    Added links to route, styles, design adjustments

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,14 @@
-import DirectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss';
+import DirectoryItem from '../directory-item/directory-item.component';
 
-const Directry = ({categories}) => {
-  return (<div className='directory-container'>
-    
-    {categories.map((category))}
-
-  </div>);
+const Directory = ({ categories }) => {
+  return (
+    <div className='directories-container'>
+      {categories.map((category) => {
+        return <DirectoryItem key={category.id} category={category} />;
+      })}
+    </div>
+  );
 };
 
-export default Directry;
+export default Directory;
