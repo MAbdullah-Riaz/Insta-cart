1f691aafce362f60ab53d624548cc263e1270717
MAbdullah-Riaz <abdullah.riaz@ocloudsolutions.net>
2023-02-01 20:09:44 +0500

    Adding responsivity of homepage

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -1,70 +1,77 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const BackgroundImage=styled.div`
-width: 100%;
-height: 100%;
-background-size: cover;
-background-position: center;
-background-image: ${({imageUrl})=>`url(${imageUrl}) `}
-`
-export const Body=styled.div`
-    height: 90px;
-    padding: 0 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid black;
-    background-color: white;
-    opacity: 0.7;
-    width: inherit;
-    position: absolute;
+export const BackgroundImage = styled.div`
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-image: ${({ imageUrl }) => `url(${imageUrl}) `};
+`;
+export const Body = styled.div`
+  height: 90px;
+  padding: 0 px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  background-color: white;
+  opacity: 0.7;
+  width: inherit;
+  position: absolute;
 
+  h2 {
+    font-weight: bold;
+    margin: 0 6px 0;
+    font-size: 22px;
+    color: #4a4a4a;
+  }
+
+  p {
+    font-weight: lighter;
+    font-size: 16px;
+  }
+  @media (max-width: 30em) {
+    & {
+      padding: 0 10px;
+    }
     h2 {
-      font-weight: bold;
-      margin: 0 6px 0;
-      font-size: 22px;
-      color: #4a4a4a;
+      font-size: 18px;
     }
-
     p {
-      font-weight: lighter;
-      font-size: 16px;
+      font-size: 12px;
     }
- 
-`
-export const DirectoryItemContainer=styled.div`
-
-
-min-width: 30%;
-height: 240px;
-flex: 1 1 auto;
-display: flex;
-align-items: center;
-justify-content: center;
-border: 1px solid black;
-margin: 0 7.5px 15px;
-overflow: hidden;
-
-&:hover {
-  cursor: pointer;
-
-  & ${BackgroundImage} {
-    transform: scale(1.1);
-    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
   }
+`;
+export const DirectoryItemContainer = styled.div`
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
 
-  & ${Body} {
-    opacity: 0.9;
-  }
-  
-  &:first-child {
-    margin-right: 7.5px;
-  }
+  &:hover {
+    cursor: pointer;
 
-  &:last-child {
-    margin-left: 7.5px;
-  }
+    & ${BackgroundImage} {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
 
-}
-`
+    & ${Body} {
+      opacity: 0.9;
+    }
+
+    &:first-child {
+      margin-right: 7.5px;
+    }
+
+    &:last-child {
+      margin-left: 7.5px;
+    }
+  }
+`;
