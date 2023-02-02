368d8bd1bfd42443052e27995cb551b8dc18e8ea
MAbdullah-Riaz <abdullah.riaz@ocloudsolutions.net>
2023-02-02 10:52:32 +0500

    correction in google button and cart rename

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -22,7 +22,7 @@ const ProductCard = ({ product }) => {
         <span className='price'>{price}</span>
       </div>
       <Button buttonType='inverted' onClick={addProductToCart}>
-        Add to card
+        Add to cart
       </Button>
     </div>
   );
